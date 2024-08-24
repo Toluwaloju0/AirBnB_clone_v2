defaa60dbd7d065da15c5da15643c98a993a6cca
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-24 21:55:19 +0100

    Airbnb Clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        # Create the list to store instances of the class
+        obj_dict = {}
+        for key, value in FileStorage.__objects.items():
+            if isinstance(value, cls):
+                obj_dict[key] = value
+        return obj_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """To delete an instance of a saved class"""
+        if obj is None:
+            return
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        del FileStorage.__objects[key]
