2e7a28976006a7f6f05da21e4f9b0bb89c28c44f
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-24 05:08:53 +0100

    AirBnB clone

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,10 +118,22 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args = args.split()
+        if args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        create_kwargs = {}
+        for a in range(1, len(args)):
+            param = args[a]
+            param = param.split("=")
+            print(param)
+            param_value = param[1].replace('_', ' ').replace('"', "")
+            param_value = str(param_value)
+            create_kwargs[param[0]] = param_value
+
+        print(create_kwargs)
+        new_instance = HBNBCommand.classes[args[0]](**create_kwargs)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -187,7 +199,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -319,6 +331,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
