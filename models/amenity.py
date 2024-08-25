5ee68b38bfc2a4dbb855b9508898198fe82bbe23
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-25 20:05:14 +0100

    Airbnb clone

--- models/amenity.py
+++ models/amenity.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from models.Place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -9,3 +10,8 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
+    place_amenity = relationship(
+        'Place',
+        secondary=place_amenity,
+        view_only=False
+    )
