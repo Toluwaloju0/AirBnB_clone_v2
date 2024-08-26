0010b3190cf9d4179491ffee5caa606453731460
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-26 18:34:31 +0100

    Airbnb clone deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 #A script to setup and configure a nginx account
-apt-get install nginx
+apt update
+apt-get install -y systemctl
+apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Hello Holberton school i am Toluwaloju Kayode" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 new_loc="\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sed -i "/server_name _;$/a $new_loc" /etc/nginx/sites-available/default
 sed -i "/n$/d" /etc/nginx/sites-available/default
-nginx -s reload
+systemctl restart nginx
+service nginx reload
